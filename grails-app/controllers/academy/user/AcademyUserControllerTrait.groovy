54ac6382bdaa3d7edccf24d400e46b27aea3573c
GusarovArtem <artemmaksimovich1@gmail.com>
2023-02-15 08:23:42 +0200

    added AcademyUserControllerTrait

---
+++ grails-app/controllers/academy/user/AcademyUserControllerTrait.groovy
@@ -0,0 +1,24 @@
+package academy.user
+
+import academy.trait.AcademyControllerTrait
+
+trait AcademyUserControllerTrait implements AcademyControllerTrait {
+
+    @Override
+    def targetDomainClass() {
+        userDomainClass()
+    }
+
+    @Override
+    void showMessage(String target, String code, String defaultCode) {
+        super.showMessage(target, code, 'User')
+    }
+
+    def _index(Integer max) {
+        def userInstanceList = targetDomainClass().findAll()
+
+        render model: [userInstanceClass: targetDomainClass(),
+                       userInstanceList : userInstanceList], view: '/user/index'
+    }
+
+}
