6f53d756858cbd04650e3b5aa7ad5d6ba14ab724
GusarovArtem <artemmaksimovich1@gmail.com>
2023-02-15 08:26:07 +0200

    added AcademyAdminController

---
+++ grails-app/controllers/academy/user/AcademyAdminController.groovy
@@ -0,0 +1,20 @@
+package academy.user
+
+import academy.user.staff.AcademyAdmin
+import grails.gorm.transactions.Transactional
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['ROLE_ADMIN'])
+@Transactional(readOnly = true)
+class AcademyAdminController implements AcademyUserControllerTrait {
+
+    def userDomainClass() {
+        return AcademyAdmin
+    }
+
+    def index(Integer max) {
+        _index(max)
+    }
+
+}
+
--- grails-app/views/index.gsp
+++ grails-app/views/index.gsp
@@ -6,7 +6,9 @@
 </head>
 <body>
 
-Welcome to Nicety Academy LMS
+<div>Welcome to Nicety Academy LMS</div>
+<br>
+<g:link controller="academyAdmin" action="index">Academy Admins</g:link>
 
 </body>
 </html>
