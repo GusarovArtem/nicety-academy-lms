2ffccb92dca3b1da54372e34bc0cba1d37d0c826
GusarovArtem <artemmaksimovich1@gmail.com>
2023-02-20 07:53:40 +0200

    added AcademyCourseController

---
+++ grails-app/controllers/academy/course/AcademyCourseController.groovy
@@ -0,0 +1,81 @@
+package academy.course
+
+import academy.trait.AcademyControllerTrait
+import grails.gorm.transactions.Transactional
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['ROLE_ADMIN'])
+@Transactional(readOnly = true)
+class AcademyCourseController implements AcademyControllerTrait {
+
+    @Override
+    def targetDomainClass() {
+        return AcademyCourse
+    }
+
+
+    def index() {
+        def courseInstanceList = targetDomainClass().findAll()
+
+        render model: [courseInstanceClass: targetDomainClass(),
+                       courseInstanceList : courseInstanceList], view: '/course/index'
+    }
+
+    def show(AcademyCourse courseInstance) {
+        courseInstance.clearErrors()
+        render model: [courseInstance: courseInstance], view: '/course/show'
+    }
+
+    def create() {
+        render model: [courseInstance: targetDomainClass()
+                .newInstance(params)], view: '/course/create'
+    }
+
+    @Transactional
+    def save(AcademyCourse course) {
+        if (!course) {
+            notFound()
+            return
+        }
+
+        course.createdOn = new Date()
+        course.clearErrors()
+        course.validate()
+
+        if (course.hasErrors()) {
+            render model: [courseInstance: course], view: '/course/create'
+            return
+        }
+
+        course.save flush: true
+
+        showMessage(course.tittle, "default.created.message")
+        redirect course
+    }
+
+
+    def edit(AcademyCourse course) {
+        render model: [courseInstance: course], view: '/course/edit'
+    }
+
+    def update(AcademyCourse course) {
+        if (course == null) {
+            notFound()
+            return
+        }
+
+        course.validate()
+
+        if (course.hasErrors()) {
+            render model: [courseInstance: course], view: '/course/edit'
+            return
+        }
+
+        course.save flush: true
+
+        showMessage(course.tittle, 'academyCourse.updated')
+        redirect course
+
+    }
+
+}
