fe7570e451c09c9891e3c4ca45915c6117b9877d
GusarovArtem <artemmaksimovich1@gmail.com>
2023-02-20 07:53:15 +0200

    added AcademyCourse domain

---
+++ grails-app/domain/academy/course/AcademyCourse.groovy
@@ -0,0 +1,28 @@
+package academy.course
+
+import academy.course.group.AcademyGroup
+
+class AcademyCourse {
+
+    String tittle
+
+    String description
+
+    Date createdOn
+
+    AcademyCourseType courseType
+
+    boolean active
+
+    static hasMany = [groups: AcademyGroup]
+
+    static constraints = {
+        tittle       blank: false
+        description  blank: false
+        createdOn    nullable: false
+        courseType   nullable: false
+    }
+
+
+}
+
---
+++ grails-app/domain/academy/course/AcademyCourseType.java
@@ -0,0 +1,9 @@
+package academy.course;
+
+enum AcademyCourseType {
+    PROGRAMMING,
+    DESIGN,
+    MANAGEMENT,
+    BUSINESS,
+    ENGLISH;
+}
