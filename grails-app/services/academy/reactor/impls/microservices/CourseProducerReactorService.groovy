7d9ee2ed7fb6dcf426262034722d1d36ee96f3b4
GusarovArtem <artemmaksimovich1@gmail.com>
2023-03-01 21:03:55 +0200

    AcademyCourse: added kafka-producer reactor impl

---
+++ grails-app/services/academy/reactor/impls/microservices/CourseProducerReactorService.groovy
@@ -0,0 +1,33 @@
+package academy.reactor.impls.microservices
+
+import academy.course.AcademyCourse
+import academy.microservices.producer.course.AcademyCourseProducer
+import academy.persist.change.ChangeType
+import academy.reactor.Reactor
+import org.springframework.beans.factory.annotation.Autowired
+
+
+class CourseProducerReactorService extends Reactor {
+
+    @Autowired
+    AcademyCourseProducer courseProducer
+
+    @Override
+    void configure() {
+        on AcademyCourse, {
+            types ChangeType.CREATED
+
+            react {
+                courseProducer.createCourse(it)
+            }
+        }
+        on AcademyCourse, {
+            types ChangeType.UPDATED
+
+            react {
+                courseProducer.updateCourse(it)
+            }
+        }
+    }
+
+}
