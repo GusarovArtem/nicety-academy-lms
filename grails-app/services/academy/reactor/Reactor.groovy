5be58b8bb0ac3f233455c99b85bff3d54dd78c74
GusarovArtem <artemmaksimovich1@gmail.com>
2023-03-03 19:25:59 +0200

    added Reactor:notify method

--- grails-app/services/academy/reactor/Reactor.groovy
+++ grails-app/services/academy/reactor/Reactor.groovy
@@ -1,5 +1,6 @@
 package academy.reactor
 
+import academy.persist.event.EntityChangedEvent
 import academy.reactor.config.DelegatesToReactorConfigurer
 import academy.reactor.config.ReactorConfig
 import groovy.util.logging.Slf4j
@@ -12,6 +13,25 @@ abstract class Reactor implements ReactorConfigTrait {
                                        @DelegatesToReactorConfigurer Closure... config) {
         onWithInitialConfig(ReactorConfig.of(clazz, domainService.allPersistentProperties(clazz)), config)
     }
+
+    boolean notify(List<EntityChangedEvent> changes) {
+        def (config, matchingEvent) = configs.findResult { c ->
+            def matchingEvent = changes.find { c.matches(it) }
+            matchingEvent ? [c, matchingEvent] : null
+        } ?: [null, null]
+
+        if (config) {
+            log.info("REACTOR ${this.class.simpleName} fired:\n\t${matchingEvent}\n\t${config}")
+            def entity = matchingEvent.entity
+            def closure = config.reactClosure
+            closure.delegate = entity
+            closure(matchingEvent)
+            return true
+        }
+
+        false
+    }
+
 
 }
 
