0f47c758c598f9495bea6423ce9f3c93deb7c539
GusarovArtem <artemmaksimovich1@gmail.com>
2023-04-12 12:40:28 +0300

    updated app configuration

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,93 +1,54 @@
-academy {
-    user {
-        super_admin {
-            name = "Artem"
-            surname = "Gusarov"
-            email = "nicety.academy@gmail.com"
-            password = "cbbcc56f20efc1a8713ad014a0"
-        }
-    }
-}
-
-// QUARTZ
-quartz {
-    autoStartup = false
-
-    globalJobListenerNames = []
-    globalTriggerListenerNames = []
-    schedulerListenerNames = []
-}
-
-// CACHE
-grails {
-    cache {
-        enabled = true
-
-        ehcache {
-            ehcacheXmlLocation = 'classpath:ehcache-cache-plugin.xml'
-            lockTimeout = 200 // In milliseconds
-        }
-    }
-}
-
-auth {
-    url = 'https://auth.academy.nicety/authenticate'
-    jwt {
-        algorithm = 'HS512'
-        secret = 'kY2iN4qrWe/VFzN1dSmpSuxzFJwJKllt4p/fv4zz1ayGGkdzzxQ2X93TH0O312S'
-    }
-}
 
 //SPRING SECURITY
 // in current version spring-security the username property might not be override-able
 //grails.plugin.springsecurity.userLookup.usernamePropertyName = 'email'
-grails.plugin.springsecurity.userLookup.userDomainClassName = 'academy.user.AcademyUser'
-grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'academy.user.security.AcademyUserRole'
-grails.plugin.springsecurity.authority.className = 'academy.user.security.AcademyRole'
+//grails.plugin.springsecurity.userLookup.userDomainClassName = 'academy.user.AcademyUser'
+//grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'academy.user.security.AcademyUserRole'
+//grails.plugin.springsecurity.authority.className = 'academy.user.security.AcademyRole'
 
-grails.plugin.springsecurity.useSwitchUserFilter = true
+//grails.plugin.springsecurity.useSwitchUserFilter = true
 
-grails.plugin.springsecurity.rememberMe.cookieName = "academyRememberMeCookieName"
-grails.plugin.springsecurity.rememberMe.key = "academyRememberMeKEY"
-grails.plugin.springsecurity.rememberMe.tokenValiditySeconds = 1209600
+//grails.plugin.springsecurity.rememberMe.cookieName = "academyRememberMeCookieName"
+//grails.plugin.springsecurity.rememberMe.key = "academyRememberMeKEY"
+//grails.plugin.springsecurity.rememberMe.tokenValiditySeconds = 1209600
 
-grails.plugin.springsecurity.useBasicAuth = true
-grails.plugin.springsecurity.basic.realmName = "Academy basic auth realm"
-grails.plugin.springsecurity.filterChain.chainMap = [
-        [pattern: '/api/errorraport',    filters: 'JOINED_FILTERS,-basicAuthenticationFilter,-basicExceptionTranslationFilter'],
-        [pattern: '/api/errorraport/**', filters: 'JOINED_FILTERS,-basicAuthenticationFilter,-basicExceptionTranslationFilter'],
-        [pattern: '/api/**',             filters: 'JOINED_FILTERS,-exceptionTranslationFilter'],
-        [pattern: '/**',                 filters: 'JOINED_FILTERS,-basicAuthenticationFilter,-basicExceptionTranslationFilter']
-]
+//grails.plugin.springsecurity.useBasicAuth = true
+//grails.plugin.springsecurity.basic.realmName = "Academy basic auth realm"
+//grails.plugin.springsecurity.filterChain.chainMap = [
+//        [pattern: '/api/errorraport',    filters: 'JOINED_FILTERS,-basicAuthenticationFilter,-basicExceptionTranslationFilter'],
+//        [pattern: '/api/errorraport/**', filters: 'JOINED_FILTERS,-basicAuthenticationFilter,-basicExceptionTranslationFilter'],
+//        [pattern: '/api/**',             filters: 'JOINED_FILTERS,-exceptionTranslationFilter'],
+//        [pattern: '/**',                 filters: 'JOINED_FILTERS,-basicAuthenticationFilter,-basicExceptionTranslationFilter']
+//]
 
-grails.plugin.springsecurity.logout.postOnly = false
-grails.plugin.springsecurity.logout.alwaysUseDefaultTargetUrl = true
+//grails.plugin.springsecurity.logout.postOnly = false
+//grails.plugin.springsecurity.logout.alwaysUseDefaultTargetUrl = true
 
-grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-        [pattern: '/',               access: ['permitAll']],
-        [pattern: '/**/show/**',     access: ['permitAll']],
-        [pattern: '/**/selfEdit/**', access: ['permitAll']],
+//grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+//        [pattern: '/',               access: ['permitAll']],
+//        [pattern: '/**/show/**',     access: ['permitAll']],
+//        [pattern: '/**/selfEdit/**', access: ['permitAll']],
 
-        [pattern: '/**/create',       access: ['ROLE_ADMIN']],
-        [pattern: '/**/save/**',   access: ['ROLE_ADMIN']],
-        [pattern: '/**/edit/**',   access: ['ROLE_ADMIN']],
-        [pattern: '/**/update/**', access: ['ROLE_ADMIN']],
-
-        [pattern: '/index',        access: ['ROLE_ADMIN', 'ROLE_MANAGER']],
-
-        [pattern: '/login/**',  access: ['permitAll']],
-        [pattern: '/logout/**', access: ['permitAll']],
-        [pattern: '/error',     access: ['permitAll']],
-
-        [pattern: '/assets/**', access: ['permitAll']],
-        [pattern: '/static/**', access: ['permitAll']],
-        [pattern: '/**/js/**',  access: ['permitAll']],
-        [pattern: '/**/css/**',      access: ['permitAll']],
-        [pattern: '/**/images/**',   access: ['permitAll']],
-        [pattern: '/**/favicon.ico', access: ['permitAll']],
-
-        [pattern: '/language',    access: ['permitAll']],
-        [pattern: '/language/**', access: ['permitAll']]
-]
-
-grails.plugin.console.csrfProtection.enabled = false
+//        [pattern: '/**/create',       access: ['ROLE_ADMIN']],
+//        [pattern: '/**/save/**',   access: ['ROLE_ADMIN']],
+//        [pattern: '/**/edit/**',   access: ['ROLE_ADMIN']],
+//        [pattern: '/**/update/**', access: ['ROLE_ADMIN']],
+//
+//        [pattern: '/index',        access: ['ROLE_ADMIN', 'ROLE_MANAGER']],
+//
+//        [pattern: '/login/**',  access: ['permitAll']],
+//        [pattern: '/logout/**', access: ['permitAll']],
+//        [pattern: '/error',     access: ['permitAll']],
+//
+//        [pattern: '/assets/**', access: ['permitAll']],
+//        [pattern: '/static/**', access: ['permitAll']],
+//        [pattern: '/**/js/**',  access: ['permitAll']],
+//        [pattern: '/**/css/**',      access: ['permitAll']],
+//        [pattern: '/**/images/**',   access: ['permitAll']],
+//        [pattern: '/**/favicon.ico', access: ['permitAll']],
+//
+//        [pattern: '/language',    access: ['permitAll']],
+//        [pattern: '/language/**', access: ['permitAll']]
+//]
+//
+//grails.plugin.console.csrfProtection.enabled = false
